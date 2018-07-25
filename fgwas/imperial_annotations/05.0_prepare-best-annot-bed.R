e05a31a584e3e5bd9a25db4f82ba1772ef68ded6
Jason Torres <jason.matthew.torres@gmail.com>
2018-07-25 15:48:33 +0100

    updated paths

--- fgwas/imperial_annotations/05.0_prepare-best-annot-bed.R
+++ fgwas/imperial_annotations/05.0_prepare-best-annot-bed.R
@@ -3,14 +3,14 @@
 library("data.table")
 library("dplyr")
 #serv.dir <- "/Users/jtorres/FUSE/"
-serv.dir <- "/well/got2d/jason/"
+serv.dir <- "/home/jason/science/servers/FUSE5/"
 
 work.dir <- serv.dir %&% "projects/islet_hubs/fgwas/imperial_annotations/"
 fgwas.output.dir <- work.dir %&% "fgwas_output/"
 cond.dir <- work.dir %&% "conditional/"
 input.dir <- cond.dir %&% "fgwas_input_files/"
 
-annot.file <- work.dir %&% "fgwas_input/" %&% "anno_input.bed"
+annot.file <- work.dir %&% "fgwas_input/" %&% "imperial-annotations.bed"#"anno_input.bed"
 annot.df <- fread(annot.file)
 
 best.df <- fread(fgwas.output.dir %&% "best-joint-model.params")
@@ -21,3 +21,4 @@ filt.annot.df <- filter(annot.df,V4 %in% annots)
 
 write.table(x=filt.annot.df,file=input.dir%&%"best_anno_input.bed",sep="\t",quote=FALSE,
             row.names=FALSE,col.names=FALSE)
+# 
